fcaceecf96f26e9b9484748343e1c0f7afbd1582
brunoholanda <holanda_rodrigues@hotmail.com>
2024-07-22 17:17:54 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '../src/auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { LeadsModule } from './leads/leads.module';
